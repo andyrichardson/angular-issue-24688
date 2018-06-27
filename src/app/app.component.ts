38fc035800a47e87eaca507a71cbbf61a78b23a9
Andy Richardson <arichardson@bravurasolutions.com>
2018-06-27 11:34:54 +0100

    add reproduciton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  loaded = false;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  get animationUrl() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      "https://media.giphy.com/media/jzaZ23z45UxK8/giphy.gif"
+    );
+  }
 }
